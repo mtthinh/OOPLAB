2e3a56f92fc59acf34e86ebe7d7836b04b4cdcd3
mtthinh <maituanthinh129@gmail.com>
2026-03-27 14:26:30 +0700

    fix

--- ShowTwoNumbers.java
+++ ShowTwoNumbers.java
@@ -5,7 +5,7 @@ public class ShowTwoNumbers {
         String strNotifocation = "You've just entered: ";
 
         strNum1 = JOptionPane.showInputDialog(null, "Please input the first number; ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
-        strNotifocation += strNum1 + "add";
+        strNotifocation += strNum1 + " and ";
 
         strNum2 = JOptionPane.showInputDialog(null, "Please input the second number; ", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
         strNotifocation += strNum2;
