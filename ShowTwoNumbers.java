ff5f35e7b43655a77dc35e2b2eaad2db4adcdebe
mtthinh <maituanthinh129@gmail.com>
2026-03-27 14:00:59 +0700

    abcdexyz

---
+++ ShowTwoNumbers.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class ShowTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+        String strNotifocation = "You've just entered: ";
+
+        strNum1 = JOptionPane.showInputDialog(null, "Please input the first number; ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
+        strNotifocation += strNum1 + "add";
+
+        strNum2 = JOptionPane.showInputDialog(null, "Please input the second number; ", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
+        strNotifocation += strNum2;
+
+        JOptionPane.showMessageDialog(null, strNotifocation, "Show two numbers", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    
+    }
+}
